581e331fee75dbe189ed113601754636969b109c
Knooowname <91739752+Knooowname@users.noreply.github.com>
2025-10-10 16:29:13 +0500

    10.10.2025_16:29
    10.10.2025_16:29

--- Source/client/src/redux/reducers/userSlice.ts
+++ Source/client/src/redux/reducers/userSlice.ts
@@ -1,22 +1,45 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import type { User } from "../../shared/types/user.types";
 
-interface initialStateUser {
+// Создание интерфейса для начального состояния userSlice
+export interface UserState {
     user: User | null,
     loading: boolean,
     error: string | null,
 }
 
-const initialState: initialStateUser = {
+// Создание начального состояния userSlice
+const initialState: UserState = {
     user: null,
     loading: false,
     error: null,
 }
 
+// Создание slice для установки и очистки userSlice из стора
 const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        
+        setUser(state, action: PayloadAction<User>) {
+            state.user = action.payload
+            state.error = null
+            state.loading = false
+        },
+        clearUser(state) {
+            state.user = null
+            state.error = null
+            state.loading = false
+        },
+        setLoading(state, action: PayloadAction<boolean>) {
+            state.loading = action.payload
+        },
+        setError(state, action: PayloadAction<string | null>) {
+            state.error = action.payload
+        }
     }
 })
+
+// Экспорт действий
+export const { setUser, clearUser, setLoading, setError } = userSlice.actions
+// Экспорт редьюсера
+export default userSlice.reducer
